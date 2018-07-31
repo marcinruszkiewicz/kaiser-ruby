edbcc178fd26c33b1c7b7c082bb830e5af8f3274
Marcin Ruszkiewicz <marcin.ruszkiewicz@polcode.net>
2018-07-31 13:58:11 +0200

    handle all poetic literals

--- lib/kaiser_ruby/rockstar_parser.rb
+++ lib/kaiser_ruby/rockstar_parser.rb
@@ -2,15 +2,33 @@ module KaiserRuby
   class RockstarParser < Parslet::Parser
     root(:poetic_literal)
 
-    # declare spaces to DRY out later definitions
-    rule(:space) { match(' ').repeat(1) }
+    # declare eol/eof/spaces to DRY out later definitions
+    rule(:space) { match('\s').repeat(1) }
+    rule(:eol) { match['\n'] }
+    rule(:eof) { any.absent? }
 
     # parts of a variable assignment
     rule(:proper_variable_name) { (match('[A-Z]') >> match('[a-z]').repeat(1)) }
-    rule(:assignment) { space >> (str('is') | str('was') | str('were')) >> space }
-    rule(:number_literal) { (match['a-z'].repeat(1) >> space.maybe).repeat.as(:number_str) }
 
-    rule(:poetic_literal) { poetic_number_literal.as(:var) }
-    rule(:poetic_number_literal) { proper_variable_name.as(:var_name) >> assignment >> number_literal.as(:var_value) }
+    # handle poetic numbers
+    rule(:number_assignment_worded) { space >> (str('is') | str('was') | str('were')) >> space }
+    rule(:number_assigmnent_shortened) { str("'s") >> space }
+    rule(:number_assignment) { number_assignment_worded | number_assigmnent_shortened }
+    rule(:number_literal) { (match['a-z'].repeat(1) >> space.maybe).repeat.as(:number_str) }
+    rule(:poetic_number_literal) { proper_variable_name.as(:var_name) >> number_assignment >> number_literal.as(:var_value) }
+
+    # poetic strings
+    rule(:string_assignment) { space >> str('says') >> space }
+    rule(:string_literal) { (any.repeat).as(:string_str) }
+    rule(:poetic_string_literal) { proper_variable_name.as(:var_name) >> string_assignment >> string_literal.as(:var_value) }
+
+    # poetic type literals
+    rule(:type_literal) { null_type_literal.as(:nil) | true_type_literal.as(:true) | false_type_literal.as(:false) }
+    rule(:true_type_literal) { str('true') | str('right') | str('yes') | str('ok') }
+    rule(:false_type_literal) { str('false') | str('wrong') | str('no') | str('lies') }
+    rule(:null_type_literal) { str('nothing') | str('nowhere') | str('nobody') | str('empty') | str('gone') }
+    rule(:poetic_type_literal) { proper_variable_name.as(:var_name) >> number_assignment >> type_literal.as(:var_value) }
+
+    rule(:poetic_literal) { (poetic_type_literal | poetic_string_literal | poetic_number_literal).as(:var) }
   end
 end
--- lib/kaiser_ruby/rockstar_transform.rb
+++ lib/kaiser_ruby/rockstar_transform.rb
@@ -1,6 +1,10 @@
 module KaiserRuby
   class RockstarTransform < Parslet::Transform
     rule(:number_str => simple(:str)) { str.to_s.split(/\s+/).map {|e| e.length % 10}.join.to_i }
+    rule(:string_str => simple(:str)) { "\"#{str}\"" }
+    rule(:true => simple(:_)) { 'true' }
+    rule(:false => simple(:_)) { 'false' }
+    rule(:nil => simple(:_)) { 'nil' }
 
     rule(:var => { :var_name => simple(:name), :var_value => simple(:value) }) do |context|
       "#{parameterize(context[:name])} = #{context[:value]}"
